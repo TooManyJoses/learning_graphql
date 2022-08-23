e1bb3557db21baedb24179487699011c960252ee
Jose Antonio Rubio <jose.rubio@gambyt.com>
2022-08-23 10:11:53 -0400

    graphql set u with schema and dummy data

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const express = require("express");
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const UserSchema = require('./schema/UserSchema');
 
 const app = express();
 
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: UserSchema,
+    graphiql: true,
+  })
+);
+
 app.listen(5050, () => {
-  console.log("listening");
+  console.log('listening');
 });
